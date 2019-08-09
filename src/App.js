63652bc00a5f889042d23d85d8c4713362b0e0d7
Ghabe Bossin <ghabe.bossin@gmail.com>
2019-08-09 08:59:33 -0700

    using scss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <nav className="Top-nav">
-        <a className="home" href="/">Ghabe Bossin</a>
-        <a href="/code">Code</a>
-        <a href="/design">Design</a>
-        <a href="/photography">Photography</a>
-        <a href="/life">Life</a>
+        <a className="nav-home" href="/">Ghabe Bossin</a>
+        <a className="nav-code" href="/code">Code</a>
+        <a className="nav-design"href="/design">Design</a>
+        <a className="nav-photography"href="/photography">Photography</a>
+        <a className="nav-life"href="/life">Life</a>
       </nav>
     </div>
   );
